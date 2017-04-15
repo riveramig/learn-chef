19f6071c37149e2f36b9a1586bd24f75194d0d90
Miguel <rivera-miguel@hotmail.com>
2017-04-15 16:38:50 -0500

    third upload

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures my_cookbook'
 long_description 'Installs/Configures my_cookbook'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends "chef-client"
+depends "apt"
+depends "ntp"
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
